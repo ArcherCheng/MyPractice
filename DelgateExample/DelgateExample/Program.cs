dbf3a73a0a64a2707cb6ef7b01e87e137259360c
ArcherCheng <archer.cheng@msa.hinet.net>
2019-09-11 19:51:03 +0800

    first commit 3

---
+++ DelgateExample/DelgateExample/Program.cs
@@ -0,0 +1,445 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+
+namespace DelgateExample
+{
+    public delegate int NumberCalc(int x, int y);
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //系統提供的委託 Action , Func<>
+            //SystemDelegate.DoExample();
+
+            //自定義委託
+            //MyDelegate.DoExample();
+
+            //使用模板方法,Callback
+            //CallBackDelegate.DoExample();
+
+            //Multicast
+            Multicast.DoExample();
+
+        }
+    }
+
+    #region System delegate method 1 Action
+
+    public class TestCalss
+    {
+        public void PrintReport()
+        {
+            Console.WriteLine("I am testing delegate");
+        }
+
+        public int Add(int x, int y)
+        {
+            int result = 0;
+            result = x + y;
+            Console.WriteLine("{0}+{1}={2}", x, y, result);
+            return result;
+        }
+        public int Sub(int x, int y)
+        {
+            int result = 0;
+            result = x - y;
+            Console.WriteLine("{0}-{1}={2}", x, y, result);
+            return result;
+        }
+    }
+
+    class SystemDelegate
+    {
+        public static void DoExample()
+        {
+            TestCalss testClass = new TestCalss();
+
+            Console.WriteLine("\n直接調用方法");
+            testClass.PrintReport(); //直接調用方法
+
+            //System delegate method 1 Action
+            Console.WriteLine("\n間接調用方法1");
+            Console.WriteLine("System delegate method 1 Action");
+
+            Action action = new Action(testClass.PrintReport); //不可以加(),加()是執行,
+            action.Invoke(); //間接調用方法
+            action(); ////間接調用方法
+
+            Type t1 = typeof(Action);
+            Console.WriteLine("Action.IsClass = {0}", t1.IsClass);
+
+            Console.WriteLine("\n間接調用方法2");
+            Console.WriteLine("System delegate method 2 Func<>");
+            //System delegate method 2 Func<>
+            int x = 100;
+            int y = 200;
+            int z = 0;
+
+            Func<int, int, int> func1 = new Func<int, int, int>(testClass.Add);
+            Func<int, int, int> func2 = new Func<int, int, int>(testClass.Sub);
+
+            z = func1(x, y);
+            Console.WriteLine(z);
+
+            z = func2(x, y);
+            Console.WriteLine(z);
+
+            Type t2 = typeof(Func<int, int, int>);
+            Console.WriteLine("Func<>.IsClass = {0}", t2.IsClass);
+
+            Console.ReadKey();
+        }
+    }
+
+    #endregion
+
+
+    #region 自定義委託
+    //自定義委託
+    public delegate double Calc(double x, double y);
+
+    public class Calculator
+    {
+ 
+        public double Add(double x, double y)
+        {
+            return x + y;
+        }
+
+        public double Sub(double x, double y)
+        {
+            return x - y;
+        }
+
+        public double Mul(double x, double y)
+        {
+            return x * y;
+        }
+
+        public double Div(double x, double y)
+        {
+            return x / y;
+        }
+    }
+
+    public class MyDelegate
+    {
+        public static void DoExample()
+        {
+            Calculator calculator = new Calculator();
+            Calc calc1 = new Calc(calculator.Add);
+            Calc calc2 = new Calc(calculator.Sub);
+            Calc calc3 = new Calc(calculator.Mul);
+            Calc calc4 = new Calc(calculator.Div);
+            double a = 100;
+            double b = 200;
+            double c = 0;
+
+            Console.WriteLine("\n自定義委託 delegate double Calc(double x, double y)");
+            Console.WriteLine("調用方法 calc1.Invoke(a,b)");
+            c = calc1.Invoke(a, b);
+            Console.WriteLine(c);
+            c = calc2.Invoke(a, b);
+            Console.WriteLine(c);
+            c = calc3.Invoke(a, b);
+            Console.WriteLine(c);
+            c = calc4.Invoke(a, b);
+            Console.WriteLine(c);
+
+            Console.WriteLine("\n調用方法 calc1(a,b)");
+            c = calc1(a, b);
+            Console.WriteLine(c);
+            c = calc2(a, b);
+            Console.WriteLine(c);
+            c = calc3(a, b);
+            Console.WriteLine(c);
+            c = calc4(a, b);
+            Console.WriteLine(c);
+
+            Console.ReadKey();
+        }
+    }
+
+    #endregion
+
+
+    #region 使用模板方法,callback
+    public class Logger
+    {
+        public void Log(Product product)
+        {
+            Console.WriteLine("Product '{0}' created at {1}. Price is {2}",product.Name, System.DateTime.UtcNow,product.Price);
+        }
+
+    }
+
+    public class Product
+    {
+        public string Name { get; set; }
+        public int Price { get; set; }
+    }
+
+    public class Box
+    {
+        public Product Product { get; set; }
+    }
+    
+    public class WrapFactory
+    {
+        //使用模板方法
+        public Box WrapProduct(Func<Product> getProduct)
+        {
+            Box box = new Box();
+            Product product = getProduct.Invoke();
+            box.Product = product;
+            return box;
+        }
+
+        //使用Callback方法
+        public Box WrapProduct(Func<Product> getProduct, Action<Product>logCallback)
+        {
+            Box box = new Box();
+            Product product = getProduct.Invoke();
+            box.Product = product;
+
+            if(product.Price > 50)
+            {
+                logCallback.Invoke(product);
+            }
+
+            return box;
+        }
+
+    }
+
+    public class ProductFactory
+    {
+        public Product MakePissa()
+        {
+            Product product = new Product
+            {
+                Name = "Pizza",
+                Price = 50
+            };
+            return product;
+        }
+
+        public Product MakeToyCar()
+        {
+            Product product = new Product
+            {
+                Name = "Toy car",
+                Price = 150
+            };
+            return product;
+
+        }
+    }
+
+    public interface IProductFactory
+    {
+        Product Make();
+    }
+
+    public class PizzaProductFactory : IProductFactory
+    {
+        public Product Make()
+        {
+            Product product = new Product()
+            {
+                Name = "Pizza",
+                Price = 50
+            };
+            return product;
+        }
+    }
+
+    public class ToycarProductFactory : IProductFactory
+    {
+        public Product Make()
+        {
+            Product product = new Product()
+            {
+                Name = "Toy car",
+                Price = 150
+            };
+            return product;
+        }
+    }
+
+
+
+    public class CallBackDelegate
+    {
+        //使用模板方法,回調方法
+        public static void DoExample()
+        {
+            //Console.WriteLine("\n使用模板方法,回調方法");
+            //ProductFactory productFactory = new ProductFactory();
+            //WrapFactory wrapFactory = new WrapFactory();
+
+            //Func<Product> func1 = new Func<Product>(productFactory.MakePissa);
+            //Func<Product> func2 = new Func<Product>(productFactory.MakeToyCar);
+
+            ////使用模板方法
+            //Console.WriteLine("\n使用模板方法");
+            //Box box1 = wrapFactory.WrapProduct(func1);
+            //Box box2 = wrapFactory.WrapProduct(func2);
+
+            //Console.WriteLine(box1.Product.Name);
+            //Console.WriteLine(box2.Product.Name);
+
+            //////使用回調方法
+            //Console.WriteLine("\n使用回調方法");
+            //Logger logger = new Logger();
+            //Action<Product> log = new Action<Product>(logger.Log);
+            //Box box3 = boxFactory.WrapProduct(func1, log);
+            //Box box4 = boxFactory.WrapProduct(func2, log);
+            //Console.WriteLine(box3.Product.Name, box3.Product.Price);
+            //Console.WriteLine(box4.Product.Name, box4.Product.Price);
+
+            //使用介面取代委託
+            ProductFactory productFactory = new ProductFactory();
+            //Wra
+
+            Console.ReadKey();
+        }
+
+    }
+    #endregion
+
+
+    #region Multicast
+    public class Student
+    {
+        public int Id { get; set; }
+        public ConsoleColor PenColor { get; set; }
+
+        public void DoHomework()
+        {
+            for (int i=0; i < 5; i++)
+            {
+                Console.ForegroundColor = this.PenColor;
+                Console.WriteLine("Student {0} doing homework {1} hours", this.Id, i);
+                Thread.Sleep(500);
+            }
+        }
+    }
+
+    public class Multicast
+    {
+        public static void DoExample()
+        {
+            Student stu1 = new Student() { Id = 1, PenColor = ConsoleColor.Yellow };
+            Student stu2 = new Student() { Id = 2, PenColor = ConsoleColor.Green };
+            Student stu3 = new Student() { Id = 3, PenColor = ConsoleColor.Red };
+
+
+            //Console.WriteLine("\n單獨調用");
+            //Action action1 = new Action(stu1.DoHomework);
+            //Action action2 = new Action(stu2.DoHomework);
+            //Action action3 = new Action(stu3.DoHomework);
+            //action1.Invoke();
+            //action2.Invoke();
+            //action3.Invoke();
+            ////action1();
+            ////action2();
+            ////action3();
+            //Console.WriteLine("\n合併調用");
+            //action1 += action2;
+            //action1 += action3;
+            //action1.Invoke();
+
+
+            //Console.WriteLine("\n同步調用,直接調用");
+            //stu1.DoHomework();
+            //stu2.DoHomework();
+            //stu3.DoHomework();
+            //for(int i = 0; i < 10; i++)
+            //{
+            //    Console.ForegroundColor = ConsoleColor.Cyan;
+            //    Console.WriteLine("Main thread {0}", i);
+            //    Thread.Sleep(500);
+            //}
+
+
+            //Console.WriteLine("\n同步調用,間接調用");
+            //Action action1 = new Action(stu1.DoHomework);
+            //Action action2 = new Action(stu2.DoHomework);
+            //Action action3 = new Action(stu3.DoHomework);
+            //action1();
+            //action2();
+            //action3();
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    Console.ForegroundColor = ConsoleColor.Cyan;
+            //    Console.WriteLine("Main thread {0}", i);
+            //    Thread.Sleep(500);
+            //}
+
+
+
+            //Console.WriteLine("\n隱式異步調用,間接調用");
+            //Action action1 = new Action(stu1.DoHomework);
+            //Action action2 = new Action(stu2.DoHomework);
+            //Action action3 = new Action(stu3.DoHomework);
+
+            //action1.BeginInvoke(null, null);
+            //action2.BeginInvoke(null, null);
+            //action3.BeginInvoke(null, null);
+
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    Console.ForegroundColor = ConsoleColor.Cyan;
+            //    Console.WriteLine("Main thread {0}", i);
+            //    Thread.Sleep(500);
+            //}
+            
+
+            //Console.WriteLine("\n顯示異步調用,間接調用 Thread");
+            //Thread thread1 = new Thread(new ThreadStart(stu1.DoHomework));
+            //Thread thread2 = new Thread(new ThreadStart(stu2.DoHomework));
+            //Thread thread3 = new Thread(new ThreadStart(stu3.DoHomework));
+
+            //thread1.Start();
+            //thread2.Start();
+            //thread3.Start();
+
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    Console.ForegroundColor = ConsoleColor.Cyan;
+            //    Console.WriteLine("Main thread {0}", i);
+            //    Thread.Sleep(500);
+            //}
+
+
+            Console.WriteLine("\n顯示異步調用,間接調用 Task");
+            Task task1 = new Task(new Action(stu1.DoHomework));
+            Task task2 = new Task(new Action(stu2.DoHomework));
+            Task task3 = new Task(new Action(stu3.DoHomework));
+            task1.Start();
+            task2.Start();
+            task3.Start();
+            for (int i = 0; i < 10; i++)
+            {
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.WriteLine("Main thread {0}", i);
+                Thread.Sleep(500);
+            }
+
+
+            //使用介面取代委託
+
+            Console.ReadKey();
+        }
+    
+    }
+    #endregion
+}
+
---
+++ EventExample/EventExample/Program.cs
@@ -0,0 +1,348 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Windows.Forms;
+
+namespace EventExample
+{
+    /// <summary>
+    /// event 事件的五大要素
+    /// event source (object, class)
+    /// event (member, event)
+    /// event subscriber (object, class)
+    /// event handler (member, callback function)
+    /// event subscribe (訂閱=>會通知訂閱的對像)(本質上是以委託類型為基礎的約定)
+    /// </summary>
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //TimerExample.DoExample();
+
+            //FromEvent.DoExample();
+            //FromEvent.DoExample2();
+            //FromEvent.DoExample3();
+
+            UdfEvent.DoExample();
+        }
+    }
+
+    #region Timer
+    public class TimerExample
+    {
+        public static void DoExample()
+        {
+            System.Timers.Timer timer = new System.Timers.Timer();  //event source => timer
+            timer.Interval = 1000;
+
+            Boy boy = new Boy(); //event subscriber class => boy
+            Girl girl = new Girl();
+
+            //兩個物件訂閱同一個事件
+            timer.Elapsed += boy.Action;  //event => timer.Elapsed
+            timer.Elapsed += girl.Action;  //+= => subscribe , event handler => girl.Action
+            timer.Elapsed += new ElapsedEventHandler(boy.Action); //早期做法
+
+            timer.Start();
+            Console.ReadKey();
+        }
+    }
+
+    //event subscriber class
+    class Boy
+    {
+        //event handler
+        internal void Action(object sender, ElapsedEventArgs e)
+        {
+            Console.WriteLine("Jump!");
+        }
+    }
+
+    //event subscriber class
+    class Girl
+    {
+        //event handler
+        internal void Action(object sender, ElapsedEventArgs e)
+        {
+            Console.WriteLine("Sing!");
+        }
+    }
+    #endregion
+
+    #region FormEvent
+    class FromEvent
+    {
+        //外部訂閱
+        public static void DoExample()
+        {
+            Form form = new Form();  //event source
+            Controller controller = new Controller(form);  //event subscriber
+            form.ShowDialog();
+        }
+
+        //自我訂閱及觸發
+        public static void DoExample2()
+        {
+            MyForm form = new MyForm();  //event source
+            form.Click += form.FromClick;  //一個委託 訂閱 一個事件
+            form.ShowDialog();
+        }
+
+        //訂閱內部成員的事件
+        public static void DoExample3()
+        {
+            MyForm3 form = new MyForm3();
+            form.ShowDialog();
+        }
+
+    }
+
+    //自我訂閱及觸發
+    class MyForm : Form
+    {
+        internal void FromClick(object sender, EventArgs e)
+        {
+            this.Text = System.DateTime.Now.ToString();
+        }
+    }
+
+    //外部訂閱
+    class Controller
+    {
+        private Form form;
+
+        public Controller(Form form)
+        {
+            if (form != null)
+            {
+                this.form = form; 
+                this.form.Click += this.FormClicked; //event & subscribe
+            }
+        }
+
+        //event handler
+        private void FormClicked(object sender, EventArgs e)
+        {
+            this.form.Text = "Hello World";
+        }
+    }
+
+    //訂閱內部成員的事件
+    class MyForm3 : Form
+    {
+        private TextBox textBox;
+        private Button button;
+
+        public MyForm3()
+        {
+            this.textBox = new TextBox();
+            this.button = new Button();
+            this.Controls.Add(this.textBox);
+            this.Controls.Add(this.button);
+            this.button.Click += this.ButtonClecked;
+            this.button.Top = 50;
+            this.button.Text = "Show Me Now";
+
+        }
+
+        private void ButtonClecked(object sender, EventArgs e)
+        {
+            this.textBox.Text = System.DateTime.Now.ToString();
+        }
+    }
+    #endregion
+
+    #region User defined event
+    public class UdfEvent
+    {
+        public static void DoExample()
+        {
+            Customer customer = new Customer(); 
+            Waiter waiter = new Waiter();
+            customer.Order += waiter.Action;  //OrderEventHandler=>Customer,OrderEventArgs
+            customer.Order2 += waiter.Action2; //EventHandler =>object,EventArgs
+            customer.Action();
+
+            ////用委派會出現以下漏洞
+            ////用委派 public OrderEventHandler Order;
+            //OrderEventArgs e = new OrderEventArgs();
+            //e.DishName = "Manhanquanxi";
+            //e.Size = "large";
+
+            //OrderEventArgs e2 = new OrderEventArgs();
+            //e2.DishName = "Manhanquanxi";
+            //e2.Size = "large";
+
+            //Customer badGuy = new Customer();
+            //badGuy.Order += waiter.Action;
+            //badGuy.Order.Invoke(customer, e);
+            //badGuy.Order.Invoke(customer, e2);
+
+
+            customer.PayBill();
+            Console.ReadLine();
+
+        }
+
+
+    }
+
+    public class OrderEventArgs:EventArgs
+    {
+        public string DishName { get; set; }
+        public string Size { get; set; }
+    }
+
+    public delegate void OrderEventHandler(Customer customer, OrderEventArgs e);
+
+    public class Customer
+    {
+        //完整事件宣告方式:
+        //private OrderEventHandler orderEventHandler;
+        //public event OrderEventHandler Order
+        //{
+        //    add
+        //    {
+        //        this.orderEventHandler += value;
+        //    }
+        //    remove
+        //    {
+        //        this.orderEventHandler -= value;
+        //    }
+        //}
+
+        //簡化事件宣告方式:
+        public event OrderEventHandler Order;
+        public event EventHandler Order2; //也可以用 EventHandler 這樣宣告
+
+        //用委派 會有漏洞危險
+        //public OrderEventHandler Order;
+
+        public double Bill { get; set; }
+
+        public void PayBill()
+        {
+            Console.WriteLine("I will pay ${0}", this.Bill);
+        }
+           
+        public void WalkIn()
+        {
+            Console.WriteLine("Walk in.");
+        }
+
+        public void SitDown()
+        {
+            Console.WriteLine( "Sit Down");
+        }
+
+        public void Think()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("let me think...");
+                Thread.Sleep(300);
+            }
+
+            //完整事件宣告方式:
+            //if(this.orderEventHandler != null)
+            //{
+            //    OrderEventArgs e = new OrderEventArgs();
+            //    e.DishName = "Fish";
+            //    e.Size = "large";
+            //    this.orderEventHandler.Invoke(this, e);
+            //}
+
+            //簡化事件宣告方式:
+            if (this.Order != null)
+            {
+                OrderEventArgs e = new OrderEventArgs();
+                e.DishName = "Fish";
+                e.Size = "large";
+                this.Order.Invoke(this, e);
+            }
+            if (this.Order2 != null)
+            {
+                OrderEventArgs e = new OrderEventArgs();
+                e.DishName = "Manhanquanxi";
+                e.Size = "large";
+                this.Order2.Invoke(this, e);
+            }
+
+            this.OnOrder("Kongpao Chicken", "small");
+        }
+
+        protected void OnOrder(string dishname,string size)
+        {
+            if (this.Order != null)
+            {
+                OrderEventArgs e = new OrderEventArgs();
+                e.DishName = dishname;
+                e.Size = size;
+                this.Order.Invoke(this, e);
+            }
+        }
+
+        public void Action()
+        {
+            // Console.ReadLine();
+            WalkIn();
+            SitDown();
+            Think();
+        }
+    }
+
+    public class Waiter
+    {
+        public void Action(Customer customer, OrderEventArgs e)
+        {
+            Console.WriteLine("I will serve you the dish: {0} size: {1}",e.DishName, e.Size );
+            double price = 10;
+            switch (e.Size)
+            {
+                case "small":
+                    price = price * 0.5;
+                    break;
+                case "large":
+                    price = price * 1.5;
+                    break;
+                default:
+                    price = price * 0.9;
+                    break;
+            }
+
+            customer.Bill += price;
+        }
+
+        internal void Action2(object sender, EventArgs e)
+        {
+            Customer customer = sender as Customer;
+            OrderEventArgs orderInfo = e as OrderEventArgs;
+
+            Console.WriteLine("I will serve you the dish: {0} size: {1}", orderInfo.DishName, orderInfo.Size);
+            double price = 10;
+            switch (orderInfo.Size)
+            {
+                case "small":
+                    price = price * 0.5;
+                    break;
+                case "large":
+                    price = price * 1.5;
+                    break;
+                default:
+                    price = price * 0.9;
+                    break;
+            }
+
+            customer.Bill += price;
+
+        }
+    }
+
+    #endregion
+
+}
